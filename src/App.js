323f13e63b8d05b7f8508c6313deab558837e3ed
KKOO727 <85313707+KKOO727@users.noreply.github.com>
2022-02-19 20:01:55 +0100

    initialized the project

---
+++ src/App.js
@@ -0,0 +1,78 @@
+// Make sure to import useState
+import React, { useEffect, useState } from "react";
+import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+	//Just a state variable we use to store our user's public wallet. Don't forget to import useState at the top.
+	const [currentAccount, setCurrentAccount] = useState('');
+	
+	const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log('Make sure you have metamask!');
+			return;
+		} else {
+			console.log('We have the ethereum object', ethereum);
+		}
+
+		// Check if we're authorized to access the user's wallet
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		// Users can have multiple authorized accounts, we grab the first one if its there!
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log('Found an authorized account:', account);
+			setCurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
+	};
+
+	// Render Methods
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja gif" />
+			<button className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
+		</div>
+	);
+
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="header-container">
+					<header>
+						<div className="left">
+							<p className="title">🐱‍👤 Ninja Name Service</p>
+							<p className="subtitle">Your immortal API on the blockchain!</p>
+						</div>
+					</header>
+				</div>
+
+				{renderNotConnectedContainer()}
+
+				<div className="footer-container">
+					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+					<a
+						className="footer-text"
+						href={TWITTER_LINK}
+						target="_blank"
+						rel="noreferrer"
+					>{`built with @${TWITTER_HANDLE}`}</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
